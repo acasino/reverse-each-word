ae35d0a2a9272d108190755385cd27b4735613ca
Andrew <andrew.casin@gmail.com>
2020-10-27 22:42:28 -0500

    commit tree 9c66cf511a322b38562bb1675073a4ff4bd2e380

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 #reverse_each_word that takes in a string argument of a sentence and returns that same sentence with each word reversed in place.
 def reverse_each_word(sentence)
-    output = []
-    array = sentence.split(' ')
-    array.collect do |word|
+    output = [] #create empty array to << new values and call at end
+    array = sentence.split(' ') #convert sentence string to an array
+    array.collect do |word| #for each word in the array, << the reverse word into output
         output << word.reverse
     end
-    output.join(' ')
+    output.join(' ') #convert output back into a string using join
 end
 
